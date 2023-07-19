74806ba5aabd8b9c5083b6b454366f053b6dc0cf
Liza <lizapikovskaya@gmail.com>
2023-07-19 19:53:28 +0300

    fix authSlice

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -23,7 +23,7 @@ const authSlice = createSlice({
         state.error = action.payload;
       })
       .addCase(logIn.fulfilled, (state, action) => {
-        state.user = action.payload;
+        state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
       })
